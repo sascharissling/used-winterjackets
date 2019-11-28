22861185a23560c6a7bd1085a62f6ca751080df8
sascharissling <sascha.rissling@gmail.com>
2019-11-28 10:47:32 +0100

    Add emotion, router, global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+
+export default function GlobalStyles() {
+  return (
+    <Global
+      styles={css`
+        *,
+        *::after,
+        *::before {
+          box-sizing: border-box;
+        }
+
+        body {
+          margin: 0;
+          font-family: "Source Sans Pro", sans-serif;
+          background: #48d1cc;
+        }
+      `}
+    />
+  );
+}
