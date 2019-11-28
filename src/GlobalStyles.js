03e8a3b8c9c90b132ccf860b0f2232e8cc0e19d5
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2019-11-28 12:12:45 +0100

    add router and connect pages (#2)

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -14,7 +14,7 @@ export default function GlobalStyles() {
         body {
           margin: 0;
           font-family: "Source Sans Pro", sans-serif;
-          background: #48d1cc;
+          background: #ffe;
         }
       `}
     />
