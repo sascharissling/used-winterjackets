c8843962644a61b8322c43aeb1a3a2e627d8a4ce
sascharissling <sascha.rissling@gmail.com>
2019-11-28 19:12:39 +0100

    styled sales form for sellers

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -1,14 +1,46 @@
 import React from "react";
 import styled from "@emotion/styled";
 
+//STYLE Start
+
 const SalesForm = styled.form`
-  width: 70%;
+  width: 90%;
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
   align-content: center;
 `;
 
+const InputField = styled.div`
+  background: #fff;
+  display: flex;
+  flex-flow: column nowrap;
+  width: 90%;
+  margin: 5px;
+  font-size: 14px;
+  * {
+    font-size: 16px;
+  }
+`;
+
+const Select = styled.select`
+  background-color: #fff;
+`;
+
+const SubmitButton = styled.button`
+  font-size: 0.8rem;
+  display: flex;
+  font-weight: bold;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  background: #49d1cc;
+  margin: 5px;
+  padding: 10px;
+  box-shadow: 3px 3px 2px 0px rgba(102, 102, 102, 1);
+  color: #ffe;
+`;
+//STYLE End
 export default function Sell() {
   const [name, setItemName] = React.useState("");
   const [brand, setItemBrand] = React.useState("");
@@ -52,90 +84,110 @@ export default function Sell() {
 
   return (
     <SalesForm onSubmit={handleSubmit}>
-      <h3>Sell Your Jacket</h3>
-      <label>
-        Jacket Name
+      <h3>Verkaufe deine Jacke</h3>
+      <InputField>
+        Name der Anzeige
         <input
           autoFocus
           type="text"
-          placeholder="Name der Anzeige"
+          placeholder="z.B. Blaue Patagonia Jacke"
           value={name}
           onChange={event => setItemName(event.target.value)}
         />
-      </label>
-      <label>
-        Jacket Brand
-        <input
-          type="text"
+      </InputField>
+      <InputField>
+        Marke deiner Jacke
+        <Select
           placeholder="Marke der Jacke"
           value={brand}
           onChange={event => setItemBrand(event.target.value)}
-        />
-      </label>
-      <label>
-        Jacket Price
+        >
+          <option value="Patagonia">Patagonia</option>
+          <option value="The North Face">The North Face</option>
+          <option value="Jack Wolfskin">Jack Wolfskin</option>
+          <option value="Quechua">Quechua</option>
+          <option value="Burton">Burton</option>
+          <option value="Mammut">Mammut</option>
+          <option value="Helly Hansen">Helly Hansen</option>
+          <option value="Andere">Andere</option>
+        </Select>
+      </InputField>
+      <InputField>
+        Was soll die Jacke kosten?
         <input
-          type="text"
-          placeholder="Marke der Jacke"
+          type="number"
+          placeholder="z.B. 100€"
           value={price}
           onChange={event => setItemPrice(event.target.value)}
         />
-      </label>
-      <label>
+      </InputField>
+      <InputField>
         Jacket Condition
-        <input
-          type="text"
+        <Select
           placeholder="Zustand der Jacke"
           value={condition}
           onChange={event => setItemCondition(event.target.value)}
-        />
-      </label>
-      <label>
+        >
+          {" "}
+          <option value="Neu">Neu</option>
+          <option value="Sehr Gut">Sehr Gut</option>
+          <option value="Gut">Gut</option>
+          <option value="Mängel">Mängel</option>
+        </Select>
+      </InputField>
+      <InputField>
         Jacket Image URL
         <input
-          type="text"
-          placeholder="Marke der Jacke"
+          type="url"
+          placeholder="z.B. http://url.de/jacke.png"
           value={img}
           onChange={event => setItemImage(event.target.value)}
         />
-      </label>
-      <label>
-        Jacket Location
+      </InputField>
+      <InputField>
+        Wo ist die Jacke?
         <input
           type="text"
-          placeholder="Marke der Jacke"
+          placeholder="z.B. Köln"
           value={location}
           onChange={event => setItemLocation(event.target.value)}
         />
-      </label>
-      <label>
+      </InputField>
+      <InputField>
         Jacket Size
-        <input
-          type="text"
+        <Select
           placeholder="Marke der Jacke"
           value={size}
           onChange={event => setItemSize(event.target.value)}
-        />
-      </label>
-      <label>
-        Jacket Description
+        >
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </Select>
+      </InputField>
+      <InputField>
+        Beschreibe deine Jacke
         <textarea
           type="text"
-          placeholder="Beschreibung der Jacke"
+          rows="4"
+          placeholder="z.B. Verkaufe diese schöne Winterjacke. Habe Sie ein Jahr getragen und brauche Sie nun nicht mehr, da ich in Thailand wohne."
           value={description}
           onChange={event => setItemDescription(event.target.value)}
         />
-      </label>
-      <label>
-        Your Contact E-Mail{" "}
+      </InputField>
+      <InputField>
+        So kann man mich erreichen
         <input
-          type="text"
+          type="email"
           placeholder="Deine Email"
           value={sellerEmail}
           onChange={event => setItemEmail(event.target.value)}
         />
-      </label>
-      <button>Submit</button>
+      </InputField>
+      <SubmitButton>Einstellen</SubmitButton>
     </SalesForm>
   );
 }
